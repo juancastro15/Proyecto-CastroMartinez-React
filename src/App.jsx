c15cf8f650e3c113222454a33ef2b10198751bb7
jccastrom <juancacastrotrader@gmail.com>
2024-06-12 19:08:15 -0400

    encontrando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Navbar } from "./components/navbar/Navbar";
+import ItemListContainer from "./pages/itemListContainer/ItemListContainer";
+import LoginContainer from "./pages/login/LoginContainer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [estaMontado, setEstaMontado] = useState(false);
+
+  const montarDesmontar = () => {
+    setEstaMontado(!estaMontado);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+
+      <LoginContainer />
+
+      {estaMontado ? <ItemListContainer greeting={"Hola como estas?"} /> : null}
+
+      <button onClick={montarDesmontar}>montar / desmontar</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
