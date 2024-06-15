bb19c473615a66a4c749eddd45ea409d25951a94
jccastrom <juancacastrotrader@gmail.com>
2024-06-15 06:26:03 -0400

    greeting text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <ItemListContainer greeting="OUR STORY" />
+      <ItemListContainer greeting="LA CABRA STYLE" />
     </>
   );
 }
--- src/pages/itemListContainer/ItemList.jsx
+++ src/pages/itemListContainer/ItemList.jsx
@@ -1,6 +1,12 @@
 const ItemList = ({ greeting }) => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
       <h1>{greeting}</h1>
     </div>
   );
